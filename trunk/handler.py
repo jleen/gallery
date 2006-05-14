5aefac41c6c00fbb5db47a78a646478fea868983
jleen <jleen@e8952cfc-6410-0410-bd16-81fef15e32ef>
2006-05-14 04:35:46 +0000

    Break the engine into components, part 2: delete most stuff from the branched files, and delete the original engine.

--- trunk/handler.py
+++ trunk/handler.py
@@ -1,15 +1,15 @@
 import cgi
 import os
 import re
-import rfc822
 import stat
 import sys
 import time
 
-sys.path.insert(0, '/home/jmleen/lib/python2.4/site-packages')
+from cache import *
+from exif import *
+from paths import *
 
 import EXIF
-from PIL import Image
 from StringIO import StringIO
 from Cheetah.Template import Template
 
@@ -21,22 +21,7 @@ big_size = "full"
 thumb_size = "200"
 preview_size = "100"
 
-def scriptdir(fname):
-    return os.path.join(os.path.split(__file__)[0], fname)
 
-cache_dependencies = [
-    gallery_config.__file__,
-    scriptdir('cache_sentinel')
-]
-scriptfiles = [
-    gallery_config.__file__,
-    scriptdir('browse.tmpl'),
-    scriptdir('exif.tmpl')
-]
-
-img_extns = ['.jpeg', '.jpg', '.JPG']
-
-trim_serials_regexp = re.compile('(^\d+_|(?<=/)\d+_)')
 
 def handler():
     reqpath = os.environ["PATH_INFO"].lower()
@@ -73,113 +58,12 @@ def photopage():
     template = Template(file=scriptdir('photopage.tmpl'), searchList=[a])
     sys.stdout.write(str(template))
 
-def path_to_url(path, size = None, ext = None):
-    rel_path = path[len(gallery_config.img_prefix):]
-    url = trim_serials(os.path.join(gallery_config.browse_prefix, rel_path))
-    (base, url_ext) = os.path.splitext(url)
-    if size != None:
-        base = base + "_" + size
-    if ext != None:
-        url_ext = "." + ext
-    return base + url_ext
-
-    
-def normalize_date(date):
-    if date == None: return None
-    return time.asctime(time.gmtime(
-        rfc822.mktime_tz(rfc822.parsedate_tz(date))))
-
-def check_client_cache(content_type, mtime):
-    mtime = max(mtime, script_mtime())
-    client_date = normalize_date(os.environ.get('HTTP_IF_MODIFIED_SINCE'))
-    client_etag = os.environ.get('HTTP_IF_NONE_MATCH')
-    server_date = time.asctime(time.gmtime(mtime))
-    if (client_date == server_date and client_etag == server_date
-            or client_date == None and client_etag == server_date
-            or client_etag == None and client_date == server_date):
-        sys.stdout.write('Status: 304 Not Modified\r\n\r\n')
-        return 1
-    else:
-        #content_type = 'text/plain'
-        sys.stdout.write("Content-type: " + content_type + "\n")
-        sys.stdout.write('Last-Modified: ' + server_date + '\n')
-        sys.stdout.write('ETag: ' + server_date + '\n')
-        sys.stdout.write('\n')
-        #sys.stdout.write('Last-Modified: ' + server_date + '\n')
-        #sys.stdout.write('ETag: ' + server_date + '\n')
-        #print 'client raw date', os.environ.get('HTTP_IF_MODIFIED_SINCE')
-        #print 'client raw etag', os.environ.get('HTTP_IF_NONE_MATCH')
-        #print "client date", client_date
-        #print 'client etag', client_etag
-        #print 'server date', server_date
-        #print 'server etag', server_date
-        #sys.stdout.write('\n')
-        return 0
-
-def infer_image_path(base):
-    for ext in img_extns:
-        if os.path.exists(gallery_config.img_prefix + base + ext): return base + ext
-    raise ValueError
-
-def script_mtime():
-    return max_mtime_for_files(scriptfiles)
-
-def lmtime(fname):
-    return os.lstat(fname)[stat.ST_MTIME]
-
-def ambiguate_filename(regexp):
-    # This is horrifically bogus.  We should have a table or something.
-    amb = ''
-    for ch in regexp:
-        if   ch == 'a': amb += '(a|\xc3\xa4)'
-        elif ch == 'o': amb += '(o|\xc3\xb6)'
-        else: amb += ch
-    return amb
-
-def degrade_filename(fn):
-    # By a similar token, this function is a crock.
-    fn = fn.replace('\xc3\xa4', 'a')
-    fn = fn.replace('\xc3\xb6', 'o')
-    return fn
-
-def infer_serial_prefix(fname, infer_suffix = 0):
-    fname = os.path.join(gallery_config.img_prefix, fname)
-    if os.path.exists(fname): return fname[len(gallery_config.img_prefix):]
-    (dir, basename) = os.path.split(fname)
-    if not os.path.exists(dir):
-        newdir = infer_serial_prefix(dir[len(gallery_config.img_prefix):])
-        dir = os.path.join(gallery_config.img_prefix, newdir)
-    candidates = os.listdir(dir)
-    r = '^(\d+_)?' + ambiguate_filename(re.escape(basename))
-    if infer_suffix:
-        r += '(' + '|'.join(img_extns) + ')'
-    r += '$'
-    r = re.compile(r)
-    found = [ f for f in candidates if r.search(f) != None ]
-    if len(found) > 0: newbasename = found[0]
-    else: newbasename = basename
-    return os.path.join(dir, newbasename)[len(gallery_config.img_prefix):]
-
-def copyIfPresent(dst, dstKey, src, srcKey):
-    if src.has_key(srcKey):
-        dst[dstKey] = src[srcKey]
-
-def fractionToDecimal(fraction):
-    pieces = fraction.split('/')
-    if len(pieces) == 2:
-        return str(float(pieces[0]) / float(pieces[1]))
-    else:
-        return fraction
-    
 def exifpage():
     fname = os.environ["PATH_INFO"][1:]
     img_index = fname.rfind('_')
     img_path = fname[:img_index]
     img_fname = os.path.join(gallery_config.img_prefix, infer_serial_prefix(img_path))
 
-    f = open(img_fname, 'rb')
-    tags = EXIF.process_file(f)
-    f.close();
     image_mtime = lmtime(img_fname)
     if check_client_cache('text/html; charset="UTF-8"', image_mtime): return
 
@@ -188,43 +72,7 @@ def exifpage():
     #ambiguate this name?
     a['title'] = os.path.basename(img_fname)
 
-    processedTags = {}
-    #copy some of the simple tags
-
-    #light source and metering mode need mappings
-    copyIfPresent(processedTags, 'Light Source', tags, 'EXIF LightSource')
-    copyIfPresent(processedTags, 'Metering Mode', tags, 'EXIF MeteringMode')
-    copyIfPresent(processedTags, 'Date Time', tags, 'EXIF DateTimeOriginal')
-    copyIfPresent(processedTags, 'Image Optimization', tags, 'MakerNote Image Optimization')
-    copyIfPresent(processedTags, 'Hue Adjustment', tags, 'MakerNote HueAdjustment')
-    copyIfPresent(processedTags, 'Exposure Time', tags, 'EXIF ExposureTime')
-    copyIfPresent(processedTags, 'Exposure Program', tags, 'EXIF ExposureProgram')
-    copyIfPresent(processedTags, 'Focus Mode', tags, 'MakerNote FocusMode')
-
-    copyIfPresent(processedTags, 'AutoFlashMode', tags, 'MakerNote AutoFlashMode')
-    copyIfPresent(processedTags, 'Image Sharpening', tags, 'MakerNote ImageSharpening')
-    copyIfPresent(processedTags, 'Tone Compensation', tags, 'MakerNote ToneCompensation')
-    copyIfPresent(processedTags, 'Flash', tags, 'EXIF Flash')
-    copyIfPresent(processedTags, 'Lighting Type', tags, 'MakerNote LightingType')
-    copyIfPresent(processedTags, 'Noise Reduction', tags, 'MakerNote NoiseReduction')
-    copyIfPresent(processedTags, 'Flash Setting', tags, 'MakerNote FlashSetting')
-    copyIfPresent(processedTags, 'Bracketing Mode', tags, 'MakerNote BracketingMode')
-    copyIfPresent(processedTags, 'ISO Setting', tags, 'MakerNote ISOSetting')
-    copyIfPresent(processedTags, 'FlashBracketCompensationApplied', tags, 'MakerNote FlashBracketCompensationApplied')
-    copyIfPresent(processedTags, 'SubSecTimeOriginal', tags, 'EXIF SubSecTimeOriginal')
-    copyIfPresent(processedTags, 'AFFocusPosition', tags, 'MakerNote AFFocusPosition')
-    copyIfPresent(processedTags, 'WhiteBalanceBias', tags, 'MakerNote WhiteBalanceBias')
-    copyIfPresent(processedTags, 'ExposureBiasValue', tags, 'EXIF ExposureBiasValue')
-    copyIfPresent(processedTags, 'Whitebalance', tags, 'MakerNote Whitebalance')
-
-
-    #Map various exif data
-
-    #fractional
-    if tags.has_key('EXIF FNumber'):
-        processedTags['FNumber'] = fractionToDecimal(tags['EXIF FNumber'].printable)
-    if tags.has_key('EXIF FocalLength'):
-        processedTags['Focal Length'] = fractionToDecimal(tags['EXIF FocalLength'].printable)
+    processedTags = exif_tags(img_fname)
 
     a['data'] = processedTags
 
@@ -247,12 +95,6 @@ def photo():
         size = int(size)
         return spewphoto(img_fname, size)
 
-def iscached(srcfile, cachefile):
-    if not os.path.isfile(cachefile): return 0
-    for dependency in [ srcfile ] + cache_dependencies:
-        if lmtime(cachefile) < lmtime(dependency):
-            return 0
-    return 1 
 def spewphoto(fname, size):
     cachedir = "%s%d" % (gallery_config.cache_prefix, size)
     cachefile = cachedir + '/' + fname
@@ -274,10 +116,6 @@ def spewfile(fname):
     sys.stdout.write(fil.read())
     fil.close()
 
-def makedirsfor(fname):
-    dirname = os.path.split(fname)[0]
-    if not os.path.isdir(dirname): os.makedirs(dirname)
-
 def first_image_fname(dir_fname):
     img_dir = os.path.join(gallery_config.img_prefix, dir_fname)
     fnames = os.listdir(img_dir)
@@ -292,88 +130,8 @@ def first_image_fname(dir_fname):
         if os.path.isdir(os.path.join(img_dir, fname)):
             return os.path.join(fname, first_image_fname(os.path.join(dir_fname, fname)))
 
-def img_size(fname, size):
-    cachedir = "%s%d" % (gallery_config.cache_prefix, size)
-    cachefile = cachedir + '/' + fname
-    srcfile = gallery_config.img_prefix + fname
-    if not iscached(srcfile, cachefile):
-        return cache_img(fname, size, cachedir, cachefile, 0)
-    else:
-        img = Image.open(cachefile)
-        return img.size
-
-def cache_img(fname, size, cachedir, cachefile, do_output):
-    img = Image.open(gallery_config.img_prefix + fname)
-    f = open(gallery_config.img_prefix + fname, 'rb')
-    tags = {}
-    try: tags = EXIF.process_file(f)
-    except: pass
-    img.thumbnail((size,size), Image.ANTIALIAS)
-
-    if gallery_config.apply_rotation:
-        orientation_tag = tags.get('Image Orientation')
-        if orientation_tag == None:
-            orientation_tag = ''
-        else:
-            orientation_tag = orientation_tag.printable
-        if orientation_tag.startswith("Rotated 90 CW"):
-            img = img.rotate(-90, Image.NEAREST)
-        elif orientation_tag.startswith("Rotated 90 CCW"):
-            img = img.rotate(90, Image.NEAREST)
-
-    buf = StringIO()
-    img.save(buf, "JPEG", quality = 95)
-    if do_output:
-        sys.stdout.write(buf.getvalue())
-    if os.path.isdir(cachedir):
-        makedirsfor(cachefile)
-        fil = file(cachefile, 'wb')
-        fil.write(buf.getvalue())
-        fil.close()
-    buf.close()
-    return img.size
-
-def script_path():
-    top_dirname = os.path.split(os.environ["SCRIPT_FILENAME"])[1]
-    rest_dirname = os.environ["PATH_INFO"][1:]
-    if len(rest_dirname) > 0:
-        path = os.path.join(top_dirname, rest_dirname[:-1])
-    else:
-        path = top_dirname
-    if os.path.split(path)[1] == 'index.html':
-        return os.path.split(path)[0]
-    else:
-        return path
-    
-
-def max_mtime_for_files(fnames):
-    max_mtime = 0
-    for fname in fnames:
-        mtime = lmtime(fname)
-        if mtime > max_mtime: max_mtime = mtime
-    return max_mtime
-
-def trim_serials(fn):
-    return trim_serials_regexp.sub('', fn)
-
-def format_fn_for_display(fn):
-    if fn == '.': return gallery_config.short_name
-    if fn.startswith('DSC_'): return ''
-    fn = fn.replace('_', ' ')
-    fn = fn.replace('~', '?')
-    fn = fn.replace("'", '&rsquo;')
-    fn = fn.replace('{o:}', '\xc3\xb6')
-    fn = fn.replace('{a:}', '\xc3\xa4')
-    return fn
-
 def send_redirect(new_url):
     sys.stdout.write("Location: http://www.saturnvalley.org" + new_url + "\n\n")
-
-def decompose_image_path(path):
-    (path, fname) = os.path.split(path)
-    (base, extn) = os.path.splitext(fname)
-    base = degrade_filename(trim_serials(base))
-    return (path, base, extn)
 
 def gallery():
     uri = os.environ["REQUEST_URI"]
