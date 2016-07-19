eb86952aabc030e69449d88e958603c7b6a6403b
jleen <jleen@e8952cfc-6410-0410-bd16-81fef15e32ef>
2016-07-19 05:31:01 +0000

    Get rc location from env

--- branches/wsgi/app.py
+++ branches/wsgi/app.py
@@ -1,9 +1,13 @@
-import configparser
+import configparser, os
 
 from gallery import handler
 
 def application(environ, start_response):
     config_data = configparser.ConfigParser()
-    config_data.read('/etc/gallery.rc')
-    config = config_data[environ['SCRIPT_NAME']]
+    config_data.read(os.environ.get('GALLERY_RC', '/etc/gallery.rc'))
+    if 'SCRIPT_NAME' in environ and environ['SCRIPT_NAME']:
+        config_section = environ['SCRIPT_NAME']
+    else:
+        config_section = '/'
+    config = config_data[config_section]
     return handler.application(environ, start_response, config)
