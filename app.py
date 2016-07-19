535fe5ed1a8ebbfb1f17fa237690ecba2f2f9dab
John Leen <363877+jleen@users.noreply.github.com>
2016-07-19 04:46:07 +0000

    Look up configuration based on script path.

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ from gallery import handler
 def application(environ, start_response):
     config_data = configparser.ConfigParser()
     config_data.read('/etc/gallery.rc')
-    config = config_data['/gallery']
+    config = config_data[environ['SCRIPT_NAME']]
     return handler.application(environ, start_response, config)
